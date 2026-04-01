72a04a4d6fde39e12f8b91ee8d3d0e8ddcaf816b
NgaLe02 <ngalee2002@gmail.com>
2026-04-01 11:36:16 +0700

    /.

--- auth-service/src/main/java/com/badmintion/authservice/controller/SsoPageController.java
+++ auth-service/src/main/java/com/badmintion/authservice/controller/SsoPageController.java
@@ -1,19 +1,25 @@
 package com.badmintion.authservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class SsoPageController {
 
+    @Value("${sso.redirect-base}")
+    private String redirectBase;
+
     @GetMapping("/sso/login")
-    public String loginPage() {
-        return "forward:/sso/login.html";
+    public String loginPage(Model model) {
+        model.addAttribute("redirectBase", redirectBase);
+        return "sso/login";
     }
 
     @GetMapping("/sso/register")
-    public String registerPage() {
-        return "forward:/sso/register.html";
+    public String registerPage(Model model) {
+        model.addAttribute("redirectBase", redirectBase);
+        return "sso/register";
     }
 }
